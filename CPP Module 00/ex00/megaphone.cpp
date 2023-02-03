a771f60c5475260ece665b8cde262df4796b38e0
Cleazyy <cleazy2002@gmail.com>
2023-02-03 20:10:44 +0100

    init CPP

---
+++ CPP Module 00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 18:34:50 by fluchten          #+#    #+#             */
+/*   Updated: 2023/02/03 19:53:06 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void	to_upper(char *str)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; av[i]; i++)
+	{
+		to_upper(av[i]);
+		std::cout << av[i];
+	}
+	std::cout << std::endl;
+	return 0;
+}
