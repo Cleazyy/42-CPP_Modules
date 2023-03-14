35b55fe51f33a340c2bc62c904aa46ccc7cbcf52
Cleazyy <cleazy2002@gmail.com>
2023-03-14 19:24:03 +0100

    push ex02

---
+++ CPP Module 01/ex02/srcs/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 19:01:05 by fluchten          #+#    #+#             */
+/*   Updated: 2023/03/14 19:22:35 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "string adress: " << &string << std::endl;
+	std::cout << "stringPTR adress: " << stringPTR << std::endl;
+	std::cout << "stringREF adress: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "string value: " << string << std::endl;
+	std::cout << "stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
+
+	return (0);
+}
