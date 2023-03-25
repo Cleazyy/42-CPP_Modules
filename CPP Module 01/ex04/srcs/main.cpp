9da7c89aae02b43f9b45b54e173e059d9ab6c1e6
Cleazyy <cleazy2002@gmail.com>
2023-03-25 11:24:07 +0100

    opti

--- CPP Module 01/ex04/srcs/main.cpp
+++ CPP Module 01/ex04/srcs/main.cpp
@@ -6,24 +6,23 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 10:38:12 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/25 10:55:07 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/25 11:23:19 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-static int	read_and_replace(std::string file, std::string s1, std::string s2)
+std::string	read_inputFile(std::string file)
 {
-	std::string	content;
-	std::string	line;
-
 	std::ifstream inputFile(file);
 	if (!inputFile.is_open())
 	{
 		std::cerr << "Failed to open '" << file << "'" << std::endl;
-		return (1);
+		return ("");
 	}
+	std::string	line;
+	std::string	content;
 	while (std::getline(inputFile, line))
 	{
 		content += line;
@@ -31,14 +30,23 @@ static int	read_and_replace(std::string file, std::string s1, std::string s2)
 			content += "\n";
 	}
 	inputFile.close();
+	return (content);
+}
 
+std::string	replace_fileContent(std::string content, std::string s1, std::string s2)
+{
+	std::string	newFileContent = content;
 	size_t pos = 0;
-	while ((pos = content.find(s1, pos)) != std::string::npos)
+	while ((pos = newFileContent.find(s1, pos)) != std::string::npos)
 	{
-		content = content.substr(0, pos) + s2 + content.substr(pos + s1.length());
+		newFileContent = newFileContent.substr(0, pos) + s2 + newFileContent.substr(pos + s1.length());
 		pos += s2.length();
 	}
+	return (newFileContent);
+}
 
+int	create_outputFile(std::string file, std::string content)
+{
 	std::ofstream outputFile(file + ".replace");
 	if (!outputFile.is_open())
 	{
@@ -47,7 +55,6 @@ static int	read_and_replace(std::string file, std::string s1, std::string s2)
 	}
 	outputFile << content;
 	outputFile.close();
-
 	return (0);
 }
 
@@ -61,7 +68,12 @@ int	main(int ac, char **av)
 	std::string	file = av[1];
 	std::string	s1 = av[2];
 	std::string	s2 = av[3];
-	if (read_and_replace(file, s1, s2) != 0)
+
+	std::string	fileContent = read_inputFile(file);
+	if (fileContent.empty())
+		return (1);
+	std::string newFileContent = replace_fileContent(fileContent, s1, s2);
+	if (create_outputFile(file, newFileContent) != 0)
 		return (1);
 	return (0);
 }
