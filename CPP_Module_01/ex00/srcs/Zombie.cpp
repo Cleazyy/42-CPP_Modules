c038da463ae4cdf71b76193f3a39dcee92ce056e
Cleazyy <cleazy2002@gmail.com>
2023-03-25 17:02:36 +0100

    fix ex00

--- CPP_Module_01/ex00/srcs/Zombie.cpp
+++ CPP_Module_01/ex00/srcs/Zombie.cpp
@@ -6,24 +6,23 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 18:05:14 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/13 19:25:36 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/25 17:00:32 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name)
-{
-	_name = name;
-	std::cout << "\033[1;32m" << _name << " was created." << "\033[0m" << std::endl;
+Zombie::Zombie(std::string name) : _name(name)
+{	
+	std::cout << "\033[1;32m" << this->_name << " was created." << "\033[0m" << std::endl;
 }
 
 Zombie::~Zombie(void)
 {
-	std::cout << "\033[1;31m" << _name << " has been destroyed." << "\033[0m" << std::endl;
+	std::cout << "\033[1;31m" << this->_name << " has been destroyed." << "\033[0m" << std::endl;
 }
 
 void	Zombie::announce(void)
 {
-	std::cout << "\033[1;33m" << _name << ": BraiiiiiiinnnzzzZ..." << "\033[0m" << std::endl;
+	std::cout << "\033[1;33m" << this->_name << ": BraiiiiiiinnnzzzZ..." << "\033[0m" << std::endl;
 }
