b4f341ff7476b4a6d101c611e363262c85c83db8
Cleazyy <cleazy2002@gmail.com>
2023-04-01 15:26:30 +0200

    init ex03

---
+++ CPP_Module_03/ex03/srcs/ScavTrap.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 19:14:30 by fluchten          #+#    #+#             */
+/*   Updated: 2023/04/01 13:41:59 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+/* ************************************************************************** */
+/*                               Canonical form                               */
+/* ************************************************************************** */
+
+ScavTrap::ScavTrap(void) : ClapTrap()
+{
+	std::cout << "ScavTrap's default constructor called" << std::endl;
+	this->_hitPoints = 100;
+	this->_energyPoints = 50;
+	this->_attackDamage = 20;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
+{
+	std::cout << "ScavTrap's set name constructor called" << std::endl;
+	this->_hitPoints = 100;
+	this->_energyPoints = 50;
+	this->_attackDamage = 20;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& rhs)
+{
+	std::cout << "ScavTrap's copy constructor called" << std::endl;
+	*this = rhs;
+}
+
+ScavTrap&	ScavTrap::operator=(const ScavTrap& rhs)
+{
+	std::cout << "ScavTrap's copy assignment operator called" << std::endl;
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_hitPoints = rhs._hitPoints;
+		this->_energyPoints = rhs._energyPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+	return (*this);
+}
+
+ScavTrap::~ScavTrap(void)
+{
+	std::cout << "ScavTrap's destructor called" << std::endl;
+}
+
+/* ************************************************************************** */
+/*                          Public Member functions                           */
+/* ************************************************************************** */
+
+void	ScavTrap::attack(const std::string& target)
+{
+	if (this->_isDead() || this->_hasNoEnergy())
+		return ;
+	std::cout << "ScavTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
+	this->_energyPoints--;
+	this->_displayHealthEnergy();
+}
+
+void	ScavTrap::guardGate(void)
+{
+	if (this->_isDead())
+		return ;
+	std::cout << "ScavTrap " << this->_name << " has entered Gate keeper mode!" << std::endl;
+}
