ebe2d025fe8fbf8ac6d276cb5053d262f5a5c654
Cleazyy <cleazy2002@gmail.com>
2023-04-01 14:15:11 +0200

    finish ex02

---
+++ CPP_Module_03/ex02/includes/FragTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/01 13:03:13 by fluchten          #+#    #+#             */
+/*   Updated: 2023/04/01 14:04:54 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_H
+# define FRAGTRAP_H
+
+# include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+	public:
+		FragTrap(void);
+		FragTrap(std::string name);
+		FragTrap(const FragTrap& rhs);
+		FragTrap& operator=(const FragTrap& rhs);
+		~FragTrap(void);
+
+		void	highFivesGuys(void);
+};
+
+#endif
---
+++ CPP_Module_03/ex02/srcs/FragTrap.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 19:14:30 by fluchten          #+#    #+#             */
+/*   Updated: 2023/04/01 14:11:40 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+/* ************************************************************************** */
+/*                               Canonical form                               */
+/* ************************************************************************** */
+
+FragTrap::FragTrap(void) : ClapTrap()
+{
+	std::cout << "FragTrap's default constructor called" << std::endl;
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
+{
+	std::cout << "FragTrap's set name constructor called" << std::endl;
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
+}
+
+FragTrap::FragTrap(const FragTrap& rhs)
+{
+	std::cout << "FragTrap's copy constructor called" << std::endl;
+	*this = rhs;
+}
+
+FragTrap&	FragTrap::operator=(const FragTrap& rhs)
+{
+	std::cout << "FragTrap's copy assignment operator called" << std::endl;
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_hitPoints = rhs._hitPoints;
+		this->_energyPoints = rhs._energyPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+	return (*this);
+}
+
+FragTrap::~FragTrap(void)
+{
+	std::cout << "FragTrap's destructor called" << std::endl;
+}
+
+/* ************************************************************************** */
+/*                          Public Member functions                           */
+/* ************************************************************************** */
+
+void	FragTrap::highFivesGuys(void)
+{
+	if (this->_isDead())
+		return ;
+	std::cout << "FragTrap " << this->_name << " asks for high five!" << std::endl;
+}
+
