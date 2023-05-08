de83a3dd1590478820a35bd6c354880f02098234
Cleazyy <cleazy2002@gmail.com>
2023-05-08 09:59:40 +0200

    init ex01

---
+++ CPP_Module_05/ex01/includes/Form.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 09:56:18 by fluchten          #+#    #+#             */
+/*   Updated: 2023/05/08 09:59:17 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+
+class Bureaucrat;
+
+class Form
+{
+
+};
+
+std::ostream&	operator<<(std::ostream& o, const Form& rhs);
+
+#endif
