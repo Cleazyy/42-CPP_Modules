cab48c891b5161e9e5c0c054e7a89aeaee25d909
Cleazyy <cleazy2002@gmail.com>
2023-05-09 11:01:05 +0200

    fix

--- CPP_Module_05/ex02/srcs/main.cpp
+++ CPP_Module_05/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:23:26 by fluchten          #+#    #+#             */
-/*   Updated: 2023/05/08 14:51:03 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/05/09 10:49:39 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,6 @@ int	main(void)
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
-	std::cout << std::endl;
 
 	return (0);
 }
