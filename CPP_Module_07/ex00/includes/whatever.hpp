c85654619462daf487aa57492cb2aa749c4233c0
Cleazyy <cleazy2002@gmail.com>
2023-05-14 16:53:58 +0200

    fix ex00

--- CPP_Module_07/ex00/includes/whatever.hpp
+++ CPP_Module_07/ex00/includes/whatever.hpp
@@ -6,14 +6,14 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:01:26 by fluchten          #+#    #+#             */
-/*   Updated: 2023/05/14 16:30:02 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/05/14 16:53:54 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP 
 
-template <typename T>
+template<typename T>
 void swap(T &a, T &b)
 {
 	T temp = a;
@@ -21,13 +21,13 @@ void swap(T &a, T &b)
 	b = temp;
 }
 
-template <typename T>
+template<typename T>
 const T &min(const T &a, const T &b)
 {
 	return (a < b ? a : b);
 }
 
-template <typename T>
+template<typename T>
 const T &max(const T &a, const T &b)
 {
 	return (a > b ? a : b);
