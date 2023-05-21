1c344b585408fcb482a1658ea69e5bcbae4fd18f
Cleazyy <cleazy2002@gmail.com>
2023-05-21 13:11:35 +0200

    fix ex00

--- CPP_Module_08/ex00/srcs/main.cpp
+++ CPP_Module_08/ex00/srcs/main.cpp
@@ -6,60 +6,36 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 12:43:37 by fluchten          #+#    #+#             */
-/*   Updated: 2023/05/21 13:02:29 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/05/21 13:11:07 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
-int	main(void)
+void	execute(int value)
 {
 	std::vector<int> numbers;
-
 	numbers.push_back(42);
 	numbers.push_back(1337);
 	numbers.push_back(19);
 	numbers.push_back(21);
 
 	try {
-		std::vector<int>::iterator res = easyfind(numbers, 42);
+		std::vector<int>::iterator res = easyfind(numbers, value);
 		std::cout << *res << " found at index " << std::distance(numbers.begin(), res) << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cout << "Error: " << e.what() << std::endl;
 	}
+}
 
-	try {
-		std::vector<int>::iterator res = easyfind(numbers, 1337);
-		std::cout << *res << " found at index " << std::distance(numbers.begin(), res) << std::endl;
-	}
-	catch (std::exception &e) {
-		std::cout << "Error: " << e.what() << std::endl;
-	}
-
-	try {
-		std::vector<int>::iterator res = easyfind(numbers, 19);
-		std::cout << *res << " found at index " << std::distance(numbers.begin(), res) << std::endl;
-	}
-	catch (std::exception &e) {
-		std::cout << "Error: " << e.what() << std::endl;
-	}
-
-	try {
-		std::vector<int>::iterator res = easyfind(numbers, 21);
-		std::cout << *res << " found at index " << std::distance(numbers.begin(), res) << std::endl;
-	}
-	catch (std::exception &e) {
-		std::cout << "Error: " << e.what() << std::endl;
-	}
-
-	try {
-		std::vector<int>::iterator res = easyfind(numbers, 95);
-		std::cout << *res << " found at index " << std::distance(numbers.begin(), res) << std::endl;
-	}
-	catch (std::exception &e) {
-		std::cout << "Error: " << e.what() << std::endl;
-	}
+int	main(void)
+{
+	execute(42);
+	execute(1337);
+	execute(19);
+	execute(21);
+	execute(95);
 
 	return (0);
 }
