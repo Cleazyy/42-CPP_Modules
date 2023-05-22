67e4a6e19fd7c1d501d015873ed231695cf6aa9b
Cleazyy <cleazy2002@gmail.com>
2023-05-22 22:36:03 +0200

    finish ex02

---
+++ CPP_Module_08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 18:26:11 by fluchten          #+#    #+#             */
+/*   Updated: 2023/05/22 21:56:40 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <iterator>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(void) : std::stack<T>() {}
+		MutantStack(const MutantStack &rhs) : std::stack<T>(rhs) {}
+		MutantStack &operator=(const MutantStack &rhs) {(void) rhs; return (*this);}
+		~MutantStack(void) {}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin(void) {return (this->c.begin());}
+		iterator end(void) {return (this->c.end());}
+
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		const_iterator begin(void) const {return (this->c.begin());}
+		const_iterator end(void) const {return (this->c.end());}
+
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		reverse_iterator rbegin(void) {return (this->c.rbegin());}
+		reverse_iterator rend(void) {return (this->c.rend()); }
+
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		const_reverse_iterator rbegin(void) const {return (this->c.rbegin());}
+		const_reverse_iterator rend(void) const {return (this->c.rend());}
+};
+
+#endif
---
+++ CPP_Module_08/ex02/srcs/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/21 12:43:37 by fluchten          #+#    #+#             */
+/*   Updated: 2023/05/22 22:35:35 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+/* int	main(void)
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	return (0);
+} */
+
+int	main(void)
+{
+	std::vector<int> mVector;
+
+	mVector.push_back(42);
+	mVector.push_back(1337);
+	mVector.push_back(19);
+	mVector.push_back(21);
+
+
+	std::vector<int>::const_iterator it = mVector.begin();
+	std::vector<int>::const_iterator ite = mVector.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	return (0);
+}
