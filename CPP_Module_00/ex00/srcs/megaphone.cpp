2fc2e98fa16b3e0fb6c3f78fb80a6419b88e427a
Cleazyy <cleazy2002@gmail.com>
2023-05-29 08:38:13 +0200

    fix phonebook (use std::string)

--- CPP_Module_00/ex00/srcs/megaphone.cpp
+++ CPP_Module_00/ex00/srcs/megaphone.cpp
@@ -6,33 +6,29 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 18:34:50 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/22 08:14:01 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/05/29 08:37:53 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-static void	to_upper(char *str)
-{
-	for (int i = 0; str[i]; i++)
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-	}
-}
-
 int	main(int ac, char **av)
 {
-	if (ac < 2)
-	{
+	if (ac < 2) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (int i = 1; av[i]; i++)
-	{
-		to_upper(av[i]);
-		std::cout << av[i];
+
+	for (int i = 1; av[i]; i++) {
+		std::string input = av[i];
+		for (size_t j = 0; j < input.length(); j++) {
+			if (std::islower(input[j])) {
+				input[j] = std::toupper(input[j]);
+			}
+		}
+		std::cout << input;
 	}
 	std::cout << std::endl;
+
 	return (0);
 }
